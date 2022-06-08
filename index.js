eb65f300a01ee9862387b83fc1290cbc886c057c
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-08 12:09:47 +0300

    Done.

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ function distanceTravelledInFeet(start, destination) {
 }
 
 function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-    if (distance < 400){
-        return distance * 0;
-    }else if (distance > 400 && distance < 2000){
-        return 0.02 * (distance - 400)
-    }else if (distance > 2000 && distance < 2500){
+    const blockDistance = distanceTravelledInFeet(start, destination);
+    
+    if (blockDistance < 400){
+        return blockDistance * 0;
+    }else if (blockDistance > 400 && blockDistance < 2000){
+        return 0.02 * (blockDistance - 400)
+    }else if (blockDistance > 2000 && blockDistance < 2500){
         return 25;
     }else {
         return 'cannot travel that far'
